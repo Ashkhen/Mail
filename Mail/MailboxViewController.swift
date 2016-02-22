4389fae184da9ce26edbcf75e00d7d3e26beac7a
Ashkhen Sargsyan <ashi@a5.io>
2016-02-21 16:09:37 -0800

    - Added enum with 4 cases to keep track of commands. - Programically added a tap gesture recognizer to reschedule view. - Added logic to hide signle message and move feed image up.

--- Mail/MailboxViewController.swift
+++ Mail/MailboxViewController.swift
@@ -18,11 +18,22 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var listImageView: UIImageView!
     @IBOutlet weak var rescheduleView: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var singleMessageView: UIView!
 
     var initialCenter: CGPoint!
     var initialCenterLaterIcon: CGPoint!
     var initialCenterArchiveIcon: CGPoint!
 
+    enum Command {
+        case Noop
+        case Archive
+        case Delete
+        case RemindLater
+        case List
+    }
+    
+    var command: Command = .Noop
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,7 +43,11 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
         rightIconImageView.alpha = 0
         leftIconImageView.alpha = 0
         listImageView.alpha = 0
-
+        rescheduleView.alpha = 0
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "onRescheduleViewTap")
+        self.rescheduleView.addGestureRecognizer(tapGesture)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,11 +55,19 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func onRescheduleViewTap() {
+        let newFeedImageFrame = CGRect(x: self.feedImageView.frame.origin.x, y: self.singleMessageView.frame.origin.y, width: self.feedImageView.frame.size.width, height: self.feedImageView.frame.size.height)
+        
+        self.rescheduleView.alpha = 0
+        UIView.animateWithDuration(0.4) { () -> Void in
+            self.feedImageView.frame = newFeedImageFrame
+        }
+    }
+    
     @IBAction func onMessagePan(sender: UIPanGestureRecognizer) {
-        var location = sender.locationInView(view)
-        var translation = sender.translationInView(view)
-        var velocity = sender.velocityInView(view)
+        let location = sender.locationInView(view)
+        let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
 
         if sender.state == UIGestureRecognizerState.Began {
             initialCenter = messageImageView.center
@@ -55,12 +78,11 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
             messageImageView.center.x = translation.x + initialCenter.x
             rightIconImageView.center.x = translation.x + initialCenterLaterIcon.x + 35
             leftIconImageView.center.x = translation.x + initialCenterArchiveIcon.x - 35
-
-            let messageCenter = CGFloat(messageImageView.center.x)
             
             // when panning left.
             if velocity.x < 0 {
                 if translation.x >= -60 && translation.x < 0 {
+                    // Grey
                     self.backgroundView.backgroundColor = UIColor.init(hexString: "bfbfbf")
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
@@ -72,17 +94,21 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                         }
                     })
                 } else if translation.x <= -60 && translation.x > -220 {
+                    command = .RemindLater
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
+                        // Yellow
                         self.backgroundView.backgroundColor = UIColor.init(hexString: "ffd320")
                         self.rightIconImageView.alpha = 1
-
                     })
                 } else if translation.x < -220 {
+                    command = .List
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
+                        // Brown
                         self.backgroundView.backgroundColor = UIColor.init(hexString: "d8a675")
                         self.rightIconImageView.alpha = 1
+                        self.rightIconImageView.image = UIImage(named: "list_icon")
                     })
                 }
             }
@@ -91,6 +117,7 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                 if translation.x > 0 && translation.x < 60 {
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
+                        // Grey
                         self.backgroundView.backgroundColor = UIColor.init(hexString: "bfbfbf")
                         UIView.animateWithDuration(0.4, animations: {
                             () -> Void in
@@ -98,30 +125,64 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
                         })
                     })
                 } else if translation.x > 60 && translation.x < 220 {
+                    command = .Archive
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
+                        // Green
                         self.backgroundView.backgroundColor = UIColor.init(hexString: "62d962")
                         self.leftIconImageView.alpha = 1
                     })
                 } else if translation.x > 220 {
+                    command = .Delete
                     UIView.animateWithDuration(0.5, animations: {
                         () -> Void in
+                        // Red
                         self.backgroundView.backgroundColor = UIColor.init(hexString: "ef540c")
-                        self.rightIconImageView.alpha = 1
+                        self.leftIconImageView.image = UIImage(named: "delete_icon")
                     })
                 }
             }
         } else if sender.state == UIGestureRecognizerState.Ended {
-            UIView.animateWithDuration(0.5, animations: {
-                () -> Void in
-                self.backgroundView.backgroundColor = UIColor.init(hexString: "bfbfbf")
-                self.messageImageView.center.x = self.initialCenter.x
-                self.rightIconImageView.center.x = self.initialCenterLaterIcon.x
-                self.rightIconImageView.alpha = 0
+            if command == .Archive {
+                let newMessageFrame = CGRect(x: 320, y: self.messageImageView.frame.origin.y, width: self.messageImageView.frame.size.width, height: self.messageImageView.frame.size.height)
+                let newFeedImageFrame = CGRect(x: self.feedImageView.frame.origin.x, y: self.singleMessageView.frame.origin.y, width: self.feedImageView.frame.size.width, height: self.feedImageView.frame.size.height)
+                
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.backgroundView.backgroundColor = UIColor.init(hexString: "62d962")
+                    self.leftIconImageView.alpha = 0
+                    self.messageImageView.frame = newMessageFrame
+                    }, completion: { (complete) -> Void in
+                        UIView.animateWithDuration(0.4, animations: { () -> Void in
+                            self.feedImageView.frame = newFeedImageFrame
+                        })
+                })
+               
+            } else if command == .RemindLater {
+                let newMessageFrame = CGRect(x: -320, y: self.messageImageView.frame.origin.y, width: self.messageImageView.frame.size.width, height: self.messageImageView.frame.size.height)
+                let newFeedImageFrame = CGRect(x: self.feedImageView.frame.origin.x, y: self.singleMessageView.frame.origin.y, width: self.feedImageView.frame.size.width, height: self.feedImageView.frame.size.height)
+                
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.backgroundView.backgroundColor = UIColor.init(hexString: "ffd320")
+                    self.rightIconImageView.alpha = 0
+                    self.messageImageView.frame = newMessageFrame
+                    }, completion: { (complete) -> Void in
+                        self.rescheduleView.alpha = 1
+//                        UIView.animateWithDuration(0.4, animations: { () -> Void in
+//                            self.feedImageView.frame = newFeedImageFrame
+//                        })
+                })
+            } else {
+                UIView.animateWithDuration(0.5, animations: {
+                    () -> Void in
+                    self.backgroundView.backgroundColor = UIColor.init(hexString: "bfbfbf")
+                    self.messageImageView.center.x = self.initialCenter.x
+                    self.rightIconImageView.center.x = self.initialCenterLaterIcon.x
+                    self.rightIconImageView.alpha = 0
 
-                self.leftIconImageView.center.x = self.initialCenterArchiveIcon.x
-                self.leftIconImageView.alpha = 0
-            })
+                    self.leftIconImageView.center.x = self.initialCenterArchiveIcon.x
+                    self.leftIconImageView.alpha = 0
+                })
+            }
         }
     }
 }
